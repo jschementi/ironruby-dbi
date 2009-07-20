936eb2a430637444f14a3c485356ca3ffcbae591
Ivan Porto Carrero <ivan@flanders.co.nz>
2009-07-21 00:07:22 +0200

    Went for a more database specific approach. should clear up some transaction mess

---
+++ playground/DataSetViewer/DataSetViewer/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace DataSetViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgSchema = new System.Windows.Forms.DataGridView();
+            this.cbTable = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgSchema)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgSchema
+            // 
+            this.dgSchema.AllowUserToAddRows = false;
+            this.dgSchema.AllowUserToDeleteRows = false;
+            this.dgSchema.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgSchema.Location = new System.Drawing.Point(12, 48);
+            this.dgSchema.Name = "dgSchema";
+            this.dgSchema.ReadOnly = true;
+            this.dgSchema.Size = new System.Drawing.Size(731, 511);
+            this.dgSchema.TabIndex = 0;
+            // 
+            // cbTable
+            // 
+            this.cbTable.FormattingEnabled = true;
+            this.cbTable.Location = new System.Drawing.Point(13, 13);
+            this.cbTable.Name = "cbTable";
+            this.cbTable.Size = new System.Drawing.Size(387, 21);
+            this.cbTable.TabIndex = 1;
+            this.cbTable.SelectedIndexChanged += new System.EventHandler(this.cbTable_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 571);
+            this.Controls.Add(this.cbTable);
+            this.Controls.Add(this.dgSchema);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.dgSchema)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgSchema;
+        private System.Windows.Forms.ComboBox cbTable;
+    }
+}
+
---
+++ playground/DataSetViewer/DataSetViewer/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DataSetViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private DataSet _schema;
+        private int _previousWidth;
+        private FormWindowState _previousState;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            DbConnection conn = new SqlConnection("data source=.\\SQLExpress;initial catalog=dbitest;user id=sa;password=Password123");
+            conn.Open();
+            _schema = new DataSet();
+            var schema = conn.GetSchema();
+
+            foreach (DataRow dataRow in schema.Rows)
+            {
+                _schema.Tables.Add(conn.GetSchema(dataRow["CollectionName"].ToString()));
+            }
+            
+            dgSchema.DataSource = _schema.Tables[0];
+            
+            cbTable.DataSource = _schema.Tables[0];
+            cbTable.DisplayMember = "CollectionName";
+            cbTable.ValueMember = "CollectionName";
+            _previousWidth = dgSchema.Width;
+            _previousState = WindowState;
+        }
+
+        private void cbTable_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dgSchema.DataSource = _schema.Tables[cbTable.SelectedIndex];
+        }
+
+        private void ResizeGrid(DataGridView dataGrid, ref int prevWidth)
+        {
+            
+            var dff = Width - (_previousWidth - 2);
+            dataGrid.Width = dataGrid.Width + dff;
+            if (prevWidth == 0)
+                prevWidth = dataGrid.Width;
+            if (prevWidth == dataGrid.Width)
+                return;
+
+            int fixedWidth = SystemInformation.VerticalScrollBarWidth +
+               dataGrid.RowHeadersWidth + 2;
+            
+            int mul = 100 * (dataGrid.Width - fixedWidth) /
+               (prevWidth - fixedWidth);
+            int columnWidth;
+            int total = 0;
+            DataGridViewColumn lastVisibleCol = null;
+
+            for (int i = 0; i < dataGrid.ColumnCount; i++)
+                if (dataGrid.Columns[i].Visible)
+                {
+                    columnWidth = (dataGrid.Columns[i].Width * mul + 50) / 100;
+                    dataGrid.Columns[i].Width =
+                       Math.Max(columnWidth, dataGrid.Columns[i].MinimumWidth);
+                    total += dataGrid.Columns[i].Width;
+                    lastVisibleCol = dataGrid.Columns[i];
+                }
+            if (lastVisibleCol == null)
+                return;
+            columnWidth = dataGrid.Width - total +
+               lastVisibleCol.Width - fixedWidth;
+            lastVisibleCol.Width =
+               Math.Max(columnWidth, lastVisibleCol.MinimumWidth);
+            prevWidth = dataGrid.Width;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState != _previousState && WindowState !=
+             FormWindowState.Minimized)
+            {
+                _previousState = WindowState;
+                ResizeGrid(dgSchema, ref _previousWidth);
+            }
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            ResizeGrid(dgSchema, ref _previousWidth);
+            
+        }
+    }
+}
